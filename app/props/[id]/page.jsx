c0b7b4ca12ad136f83285135d3b5a6a6a3874316
bdimon <dmytr0b@yahoo.com>
2024-08-17 16:52:32 +0200

    use client for useParams... from next/navigation

--- app/props/[id]/page.jsx
+++ app/props/[id]/page.jsx
@@ -1,10 +1,19 @@
+'use client';
 import React from 'react';
 import Link from "next/link";
+import { useParams, usePathname, useSearchParams } from 'next/navigation';
 
 function DynamicPage() {
-    return (
+    // console.log('Server Side');
+    const params = useParams();
+    // path=/props/185 => Dynamic Page 185
+    const searchParams = useSearchParams();
+    const name = searchParams.get("name");
+    // path=/props/185?name=react => Dynamic Page 185, react
+    const usePath = usePathname()
+;    return (
     <div className='bg-red-400'>        
-        <div>Dynamic Page</div>
+        <div>Dynamic Page {params.id},  {name}, {usePath}</div>
         <br />
         <Link href="/">Back to Home</Link>
         <br />
