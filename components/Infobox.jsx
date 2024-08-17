a2de487362666050c8626ee7a0f1760485b2dbee
bdimon <dmytr0b@yahoo.com>
2024-08-17 20:42:33 +0200

    stylish infoboxes

--- components/Infobox.jsx
+++ components/Infobox.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
-
-function Infobox(props) {
+import Link from 'next/link';
+// Dynamic views via propses
+function Infobox({
+  heading,
+  backgroundColor = "bg-gray-100",
+  textColor = "text-zinc-700",
+  buttonInfo,
+  children,
+}) {
     return (
-        <div>
-            
+        <div className={`${backgroundColor} p-6 rounded-lg shadow-md`}>
+          <h2 className={`${textColor} text-2xl font-bold`}>{heading}</h2>
+          <p className={`${textColor} mt-2 mb-4`}>{children}</p>
+          <Link
+            href={buttonInfo.link}
+            className={`inline-block ${buttonInfo.backgroundColor} text-zinc-700 rounded-lg px-4 py-2 hover:opacity-80`}
+          >
+            {buttonInfo.text}
+          </Link>
         </div>
-    );
+      );
 }
 
 export default Infobox;
--- components/Infoboxes.jsx
+++ components/Infoboxes.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import Infobox from './Infobox';
 
 function Infoboxes(props) {
     return (
-        <div>
-            
+        // <>
+    <section>
+      <div className="container-xl lg:container m-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+            <Infobox 
+            // dynamic views via propses
+            heading="For Clients"
+            backgroundColor="bg-gray-100"
+            buttonInfo={{
+              text: "Find subject",
+              link: "/props",
+              backgroundColor: "bg-amber-300",
+            }}
+            >
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum dolorum veniam consequatur? Vel, unde aspernatur nobis officiis ipsa aut maxime rerum ratione doloribus repudiandae id! Nostrum aliquid magnam illo sequi!
+        </Infobox>
+        <Infobox 
+            heading="For Customers"
+            backgroundColor="bg-emerald-100"
+            buttonInfo={{
+              text: "Add subject",
+              link: "/props/add",
+              backgroundColor: "bg-emerald-400",
+            }}
+            >
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum dolorum veniam consequatur? Vel, unde aspernatur nobis officiis ipsa aut maxime rerum ratione doloribus repudiandae id! Nostrum aliquid magnam illo sequi!
+        </Infobox>
         </div>
+      </div>
+    </section>
+      
+        
+        
+        // </>
     );
 }
 
