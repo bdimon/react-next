74a6ff34946a5543ef75fda0a368f864f0aa567e
bdimon <dmytr0b@yahoo.com>
2024-08-18 00:04:08 +0200

    add Propscard

--- app/props/page.jsx
+++ app/props/page.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import Link from "next/link";
+import propses from "@/propses.json"
+import Propscard from "@/components/Propscard"
 
 function PropsPage() {
     return (
-    <div className='bg-red-200'>        
-        <div>
-            Props Page
-        </div>
-        <br />
-        <br />
-        <Link href="/">Back to Home</Link>
-    </div>
+        <section className="px-4 py-6">
+            <div className="container-xl lg:container m-auto px-4 py-6">
+              {propses.length === 0 ? (<p>Nothing found</p>) : (
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                  {
+                  propses.map(
+                    (property, index) => (
+                    <Propscard key={index} property={property}/>
+                  ))
+                  }
+                </div>
+              
+              )}
+            </div>
+        </section>
+    
     );
 }
 
