85eb1d55912baeb5b9d1da24f79f1d061793843b
bdimon <dmytr0b@yahoo.com>
2024-08-18 13:18:55 +0200

    edit api router

--- app/api/properties/route.jsx
+++ app/api/properties/route.jsx
@@ -1,12 +1,8 @@
 export const GET = async (request) => {
   try {
-    return new Response(JSON.stringify({ message: "Hello World" }), {
-      status: 200,
-    });
-  } catch (error) {
+    return new Response(JSON.stringify({ message: "Hello World", status: 200 } ))  
+} catch (error) {
     console.log(error);
-    return new Response(JSON.stringify({ message: "Something went wrong" }), {
-      status: 500,
-    });
-  }
+    return new Response(JSON.stringify({ message: "Something went wrong", status: 500 }))
+}
 };
