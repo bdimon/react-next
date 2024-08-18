299f89e420cc5981dd87786762005c426b8aab87
bdimon <dmytr0b@yahoo.com>
2024-08-18 12:17:57 +0200

    add mongodb

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import Infoboxes from '@/components/Infoboxes';
 import Homeproperties from '@/components/Homeproperties';
 
 export default function Home() {
+  // console.log(process.env.MONGODB_URI);
+  
   return (
     <main>
       <Mainform />
